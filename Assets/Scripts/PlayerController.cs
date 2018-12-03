e2809f92e2f318c76cfa982e31d338240282acd4
Danar Hikmah Krismanto <165150207111036@mail.ub.ac.id>
2018-12-03 18:07:29 +0700

    Moving Player
    Baru Moving player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public node rightNow = null;
+    public string arah = "bottom";
+    bool selectDirection = false;
+
+    void Start () {
+        StartCoroutine(Move(3));
+	}
+
+    private void Update()
+    {
+        
+    }
+
+    void Gerak(node next)
+    {
+        transform.position = next.transform.position;
+    }
+
+    void WaitUntilChooseDirection()
+    {
+        Debug.Log("waitiinggg");
+        selectDirection = true;
+    }
+
+    IEnumerator Move(int jmlDadu)
+    {
+        for (int i = 0; i < jmlDadu; i++)
+        {
+            if (!selectDirection)
+            {
+                switch (arah)
+                {
+                    case "bottom":
+                        if (rightNow.bottom != null)
+                        {
+                            Gerak(rightNow.bottom);
+                            rightNow = rightNow.bottom;
+                        }
+                        else
+                        {
+                            WaitUntilChooseDirection();
+                        }
+                        break;
+                    case "top":
+                        if (rightNow.top != null)
+                        {
+                            Gerak(rightNow.top);
+                            rightNow = rightNow.top;
+                        }
+                        else
+                        {
+                            WaitUntilChooseDirection();
+                        }
+                        break;
+                    case "right":
+                        if (rightNow.right != null)
+                        {
+                            Gerak(rightNow.right);
+                            rightNow = rightNow.right;
+                        }
+                        else
+                        {
+                            WaitUntilChooseDirection();
+                        }
+                        break;
+                    case "left":
+                        if (rightNow.left != null)
+                        {
+                            Gerak(rightNow.left);
+                            rightNow = rightNow.left;
+                        }
+                        else
+                        {
+                            WaitUntilChooseDirection();
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                break;
+            }
+            yield return new WaitForSeconds(.75f);
+        }
+    }
+}
---
+++ Assets/Scripts/node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class node : MonoBehaviour {
+
+    public node top;
+    public node bottom;
+    public node right;
+    public node left;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
